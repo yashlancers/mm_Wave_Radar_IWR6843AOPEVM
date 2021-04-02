0ea2427f2035d07406ec0b295afb5a0a757be33e
yashlancers <yashlancers@hotmail.com>
2021-04-02 16:57:22 +0530

    Folder files

---
+++ radar_data_dump.py
@@ -0,0 +1,12 @@
+import time
+import os
+
+# sudoPassword = 'mypass'
+t = time.strftime('%Y-%m-%d-Time-%H-%M', time.localtime(time.time()))
+command = 'jpnevulator -r -g -e 50000 -t /dev/ttyUSB1  | tee ' + '/home/pi/mm_wave_radar/data/data'+ t +'.txt'
+# p = os.system('echo %s|sudo -S %s' % (sudoPassword, command))
+p = os.system('sudo %s' % (command))
+
+
+
+ 
---
+++ radar_serial_capture.py
@@ -0,0 +1,111 @@
+import serial
+import time
+import numpy as np
+
+# Change the configuration file name
+configFileName = 'config_file.cfg'
+CLIport = {}
+Dataport = {}
+byteBuffer = np.zeros(2**15,dtype = 'uint8')
+byteBufferLength = 0;
+
+
+# ------------------------------------------------------------------
+
+# Function to configure the serial ports and send the data from
+# the configuration file to the radar
+def serialConfig(configFileName):
+    
+    global CLIport
+    global Dataport
+    # Open the serial ports for the configuration and the data ports
+    
+    # Raspberry pi
+    CLIport = serial.Serial('/dev/ttyUSB0', 115200)
+    Dataport = serial.Serial('/dev/ttyUSB1', 921600)
+    
+    # Windows
+    #CLIport = serial.Serial('COM3', 115200)
+    #Dataport = serial.Serial('COM4', 921600)
+
+    # Read the configuration file and send it to the board
+    config = [line.rstrip('\r\n') for line in open(configFileName)]
+    for i in config:
+        CLIport.write((i+'\n').encode())
+        print(i)
+        time.sleep(0.01)
+        
+    return CLIport, Dataport
+
+# ------------------------------------------------------------------
+
+# Function to parse the data inside the configuration file
+def parseConfigFile(configFileName):
+    configParameters = {} # Initialize an empty dictionary to store the configuration parameters
+    
+    # Read the configuration file and send it to the board
+    config = [line.rstrip('\r\n') for line in open(configFileName)]
+    for i in config:
+        
+        # Split the line
+        splitWords = i.split(" ")
+        
+        # Hard code the number of antennas, change if other configuration is used
+        numRxAnt = 4
+        numTxAnt = 2
+        
+        # Get the information about the profile configuration
+        if "profileCfg" in splitWords[0]:
+            startFreq = int(float(splitWords[2]))
+            idleTime = int(splitWords[3])
+            rampEndTime = float(splitWords[5])
+            freqSlopeConst = float(splitWords[8])
+            numAdcSamples = int(splitWords[10])
+            numAdcSamplesRoundTo2 = 1;
+            
+            while numAdcSamples > numAdcSamplesRoundTo2:
+                numAdcSamplesRoundTo2 = numAdcSamplesRoundTo2 * 2;
+                
+            digOutSampleRate = int(splitWords[11]);
+            
+        # Get the information about the frame configuration    
+        elif "frameCfg" in splitWords[0]:
+            
+            chirpStartIdx = int(splitWords[1]);
+            chirpEndIdx = int(splitWords[2]);
+            numLoops = int(splitWords[3]);
+            numFrames = int(splitWords[4]);
+            framePeriodicity = int(splitWords[5]);
+
+            
+    # Combine the read data to obtain the configuration parameters           
+    numChirpsPerFrame = (chirpEndIdx - chirpStartIdx + 1) * numLoops
+    configParameters["numDopplerBins"] = numChirpsPerFrame / numTxAnt
+    configParameters["numRangeBins"] = numAdcSamplesRoundTo2
+    configParameters["rangeResolutionMeters"] = (3e8 * digOutSampleRate * 1e3) / (2 * freqSlopeConst * 1e12 * numAdcSamples)
+    configParameters["rangeIdxToMeters"] = (3e8 * digOutSampleRate * 1e3) / (2 * freqSlopeConst * 1e12 * configParameters["numRangeBins"])
+    configParameters["dopplerResolutionMps"] = 3e8 / (2 * startFreq * 1e9 * (idleTime + rampEndTime) * 1e-6 * configParameters["numDopplerBins"] * numTxAnt)
+    configParameters["maxRange"] = (300 * 0.9 * digOutSampleRate)/(2 * freqSlopeConst * 1e3)
+    configParameters["maxVelocity"] = 3e8 / (4 * startFreq * 1e9 * (idleTime + rampEndTime) * 1e-6 * numTxAnt)
+    
+    return configParameters
+   
+# ------------------------------------------------------------------
+
+
+# -------------------------    MAIN   -----------------------------------------  
+
+# Configurate the serial port
+CLIport, Dataport = serialConfig(configFileName)
+
+# Get the configuration parameters from the configuration file
+configParameters = parseConfigFile(configFileName)
+   
+# Main loop 
+detObj = {}  
+frameData = {}    
+currentIndex = 0
+        
+    
+
+
