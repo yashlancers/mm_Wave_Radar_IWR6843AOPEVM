c190995f85154b97d0067147860a7518dad71f91
yashlancers <yashlancers@hotmail.com>
2021-04-06 23:26:18 +0530

    Update folder content

---
+++ mmwave_to_mavlink.py
@@ -0,0 +1,399 @@
+
+import os
+os.environ["MAVLINK20"] = "1"
+
+import serial
+import time
+import numpy as np
+from apscheduler.schedulers.background import BackgroundScheduler
+from pymavlink import mavutil
+import threading
+import time
+import sys
+
+# sensor parameters
+DEPTH_RANGE_M = [0.1, 50]
+
+# Default configurations for connection to the FCU
+connection_string = '/dev/ttyS0'
+connection_baudrate = 921600
+
+obstacle_distance_msg_hz = 60
+
+start_time =  int(round(time.time() * 1000))
+current_milli_time = lambda: int(round(time.time() * 1000) - start_time)
+
+mavlink_x = []
+mavlink_y = []
+mavlink_z = []
+
+######################################################
+##  Functions - MAVLink                             ##
+######################################################
+
+def mavlink_loop(conn, callbacks):
+    '''a main routine for a thread; reads data from a mavlink connection,
+    calling callbacks based on message type received.
+    '''
+    interesting_messages = list(callbacks.keys())
+    while True:
+        # send a heartbeat msg
+        conn.mav.heartbeat_send(mavutil.mavlink.MAV_TYPE_ONBOARD_CONTROLLER,
+                                mavutil.mavlink.MAV_AUTOPILOT_GENERIC,
+                                0,
+                                0,
+                                0)
+        m = conn.recv_match(type=interesting_messages, timeout=1, blocking=True)
+        if m is None:
+            continue
+        callbacks[m.get_type()](m)
+
+def send_obstacle_distance_3D_message():
+    global mavlink_x, mavlink_y, mavlink_z
+
+    current_time_ms = current_milli_time()
+
+    for i in range(len(mavlink_x)):
+        conn.mav.obstacle_distance_3d_send(
+            current_time_ms,    # us Timestamp (UNIX time or time since system boot)
+            0,
+            mavutil.mavlink.MAV_FRAME_BODY_FRD,
+            65535,
+            float(mavlink_x[i]),
+            float(mavlink_y[i]),
+            float(mavlink_z[i]),
+            float(DEPTH_RANGE_M[0]),
+            float(DEPTH_RANGE_M[1])
+        )
+
+def send_msg_to_gcs(text_to_be_sent):
+    # MAV_SEVERITY: 0=EMERGENCY 1=ALERT 2=CRITICAL 3=ERROR, 4=WARNING, 5=NOTICE, 6=INFO, 7=DEBUG, 8=ENUM_END
+    text_msg = 'D4xx: ' + text_to_be_sent
+    conn.mav.statustext_send(mavutil.mavlink.MAV_SEVERITY_INFO, text_msg.encode())
+
+# Request a timesync update from the flight controller, for future work.
+# TODO: Inspect the usage of timesync_update 
+def update_timesync(ts=0, tc=0):
+    if ts == 0:
+        ts = int(round(time.time() * 1000))
+    conn.mav.timesync_send(tc, ts)
+
+
+
+
+conn = mavutil.mavlink_connection(
+    device = str(connection_string),
+    autoreconnect = True,
+    source_system = 1,
+    source_component = 93,
+    baud=connection_baudrate,
+    force_connected=True,
+)
+
+
+mavlink_callbacks = {
+}
+mavlink_thread = threading.Thread(target=mavlink_loop, args=(conn, mavlink_callbacks))
+mavlink_thread.start()
+
+# Send MAVlink messages in the background at pre-determined frequencies
+sched = BackgroundScheduler()
+
+sched.add_job(send_obstacle_distance_3D_message, 'interval', seconds = 1/obstacle_distance_msg_hz)
+send_msg_to_gcs('Sending obstacle distance messages to FCU')
+
+
+sched.start()
+
+# ----------------------RADAR CODE-----------------------------
+
+
+# Change the configuration file name
+configFileName = '/home/pi/mm_wave_radar/config_file.cfg'
+CLIport = {}
+Dataport = {}
+byteBuffer = np.zeros(2**15,dtype = 'uint8')
+byteBufferLength = 0;
+
+
+# ------------------------------------------------------------------
+
+# Function to configure the serial ports and send the data from
+# the configuration file to the radar
+def serialConfig(configFileName):
+    
+    global CLIport
+    global Dataport
+    # Open the serial ports for the configuration and the data ports
+    
+    # Raspberry pi
+    CLIport = serial.Serial('/dev/ttyUSB0', 115200)
+    Dataport = serial.Serial('/dev/ttyUSB1', 921600)
+    
+    # Windows
+    #CLIport = serial.Serial('COM3', 115200)
+    #Dataport = serial.Serial('COM4', 921600)
+
+    # Read the configuration file and send it to the board
+    config = [line.rstrip('\r\n') for line in open(configFileName)]
+    for i in config:
+        CLIport.write((i+'\n').encode())
+        print(i)
+        time.sleep(0.01)
+        
+    return CLIport, Dataport
+
+# ------------------------------------------------------------------
+
+# Function to parse the data inside the configuration file
+def parseConfigFile(configFileName):
+    configParameters = {} # Initialize an empty dictionary to store the configuration parameters
+    
+    # Read the configuration file and send it to the board
+    config = [line.rstrip('\r\n') for line in open(configFileName)]
+    for i in config:
+        
+        # Split the line
+        splitWords = i.split(" ")
+        
+        # Hard code the number of antennas, change if other configuration is used
+        numRxAnt = 4
+        numTxAnt = 3
+        
+        # Get the information about the profile configuration
+        if "profileCfg" in splitWords[0]:
+            startFreq = int(float(splitWords[2]))
+            idleTime = int(splitWords[3])
+            rampEndTime = float(splitWords[5])
+            freqSlopeConst = float(splitWords[8])
+            numAdcSamples = int(splitWords[10])
+            numAdcSamplesRoundTo2 = 1;
+            
+            while numAdcSamples > numAdcSamplesRoundTo2:
+                numAdcSamplesRoundTo2 = numAdcSamplesRoundTo2 * 2;
+                
+            digOutSampleRate = int(splitWords[11]);
+            
+        # Get the information about the frame configuration    
+        elif "frameCfg" in splitWords[0]:
+            
+            chirpStartIdx = int(splitWords[1]);
+            chirpEndIdx = int(splitWords[2]);
+            numLoops = int(splitWords[3]);
+            numFrames = int(splitWords[4]);
+            framePeriodicity = int(splitWords[5]);
+
+            
+    # Combine the read data to obtain the configuration parameters           
+    numChirpsPerFrame = (chirpEndIdx - chirpStartIdx + 1) * numLoops
+    configParameters["numDopplerBins"] = numChirpsPerFrame / numTxAnt
+    configParameters["numRangeBins"] = numAdcSamplesRoundTo2
+    configParameters["rangeResolutionMeters"] = (3e8 * digOutSampleRate * 1e3) / (2 * freqSlopeConst * 1e12 * numAdcSamples)
+    configParameters["rangeIdxToMeters"] = (3e8 * digOutSampleRate * 1e3) / (2 * freqSlopeConst * 1e12 * configParameters["numRangeBins"])
+    configParameters["dopplerResolutionMps"] = 3e8 / (2 * startFreq * 1e9 * (idleTime + rampEndTime) * 1e-6 * configParameters["numDopplerBins"] * numTxAnt)
+    configParameters["maxRange"] = (300 * 0.9 * digOutSampleRate)/(2 * freqSlopeConst * 1e3)
+    configParameters["maxVelocity"] = 3e8 / (4 * startFreq * 1e9 * (idleTime + rampEndTime) * 1e-6 * numTxAnt)
+    
+    return configParameters
+   
+# ------------------------------------------------------------------
+
+# Funtion to read and parse the incoming data
+def readAndParseData18xx(Dataport, configParameters):
+    global byteBuffer, byteBufferLength
+    
+    # Constants
+    OBJ_STRUCT_SIZE_BYTES = 12;
+    BYTE_VEC_ACC_MAX_SIZE = 2**15;
+    MMWDEMO_UART_MSG_DETECTED_POINTS = 1;
+    MMWDEMO_UART_MSG_RANGE_PROFILE   = 2;
+    maxBufferSize = 2**15;
+    tlvHeaderLengthInBytes = 8;
+    pointLengthInBytes = 16;
+    magicWord = [2, 1, 4, 3, 6, 5, 8, 7]
+    
+    # Initialize variables
+    magicOK = 0 # Checks if magic number has been read
+    dataOK = 0 # Checks if the data has been read correctly
+    frameNumber = 0
+    detObj = {}
+    
+    readBuffer = Dataport.read(Dataport.in_waiting)
+    byteVec = np.frombuffer(readBuffer, dtype = 'uint8')
+    byteCount = len(byteVec)
+    
+    # Check that the buffer is not full, and then add the data to the buffer
+    if (byteBufferLength + byteCount) < maxBufferSize:
+        byteBuffer[byteBufferLength:byteBufferLength + byteCount] = byteVec[:byteCount]
+        byteBufferLength = byteBufferLength + byteCount
+        
+    # Check that the buffer has some data
+    if byteBufferLength > 16:
+        
+        # Check for all possible locations of the magic word
+        possibleLocs = np.where(byteBuffer == magicWord[0])[0]
+
+        # Confirm that is the beginning of the magic word and store the index in startIdx
+        startIdx = []
+        for loc in possibleLocs:
+            check = byteBuffer[loc:loc+8]
+            if np.all(check == magicWord):
+                startIdx.append(loc)
+               
+        # Check that startIdx is not empty
+        if startIdx:
+            
+            # Remove the data before the first start index
+            if startIdx[0] > 0 and startIdx[0] < byteBufferLength:
+                byteBuffer[:byteBufferLength-startIdx[0]] = byteBuffer[startIdx[0]:byteBufferLength]
+                byteBuffer[byteBufferLength-startIdx[0]:] = np.zeros(len(byteBuffer[byteBufferLength-startIdx[0]:]),dtype = 'uint8')
+                byteBufferLength = byteBufferLength - startIdx[0]
+                
+            # Check that there have no errors with the byte buffer length
+            if byteBufferLength < 0:
+                byteBufferLength = 0
+                
+            # word array to convert 4 bytes to a 32 bit number
+            word = [1, 2**8, 2**16, 2**24]
+            
+            # Read the total packet length
+            totalPacketLen = np.matmul(byteBuffer[12:12+4],word)
+            
+            # Check that all the packet has been read
+            if (byteBufferLength >= totalPacketLen) and (byteBufferLength != 0):
+                magicOK = 1
+    
+    # If magicOK is equal to 1 then process the message
+    if magicOK:
+        # word array to convert 4 bytes to a 32 bit number
+        word = [1, 2**8, 2**16, 2**24]
+        
+        # Initialize the pointer index
+        idX = 0
+        
+        # Read the header
+        magicNumber = byteBuffer[idX:idX+8]
+        idX += 8
+        version = format(np.matmul(byteBuffer[idX:idX+4],word),'x')
+        idX += 4
+        totalPacketLen = np.matmul(byteBuffer[idX:idX+4],word)
+        idX += 4
+        platform = format(np.matmul(byteBuffer[idX:idX+4],word),'x')
+        idX += 4
+        frameNumber = np.matmul(byteBuffer[idX:idX+4],word)
+        idX += 4
+        timeCpuCycles = np.matmul(byteBuffer[idX:idX+4],word)
+        idX += 4
+        numDetectedObj = np.matmul(byteBuffer[idX:idX+4],word)
+        idX += 4
+        numTLVs = np.matmul(byteBuffer[idX:idX+4],word)
+        idX += 4
+        subFrameNumber = np.matmul(byteBuffer[idX:idX+4],word)
+        idX += 4
+
+        # Read the TLV messages
+        for tlvIdx in range(numTLVs):
+            
+            # word array to convert 4 bytes to a 32 bit number
+            word = [1, 2**8, 2**16, 2**24]
+
+            # Check the header of the TLV message
+            tlv_type = np.matmul(byteBuffer[idX:idX+4],word)
+            idX += 4
+            tlv_length = np.matmul(byteBuffer[idX:idX+4],word)
+            idX += 4
+
+            # Read the data depending on the TLV message
+            if tlv_type == MMWDEMO_UART_MSG_DETECTED_POINTS:
+
+                # Initialize the arrays
+                x = np.zeros(numDetectedObj,dtype=np.float32)
+                y = np.zeros(numDetectedObj,dtype=np.float32)
+                z = np.zeros(numDetectedObj,dtype=np.float32)
+                velocity = np.zeros(numDetectedObj,dtype=np.float32)
+                
+                for objectNum in range(numDetectedObj):
+                    
+                    # Read the data for each object
+                    x[objectNum] = byteBuffer[idX:idX + 4].view(dtype=np.float32)
+                    idX += 4
+                    y[objectNum] = byteBuffer[idX:idX + 4].view(dtype=np.float32)
+                    idX += 4
+                    z[objectNum] = byteBuffer[idX:idX + 4].view(dtype=np.float32)
+                    idX += 4
+                    velocity[objectNum] = byteBuffer[idX:idX + 4].view(dtype=np.float32)
+                    idX += 4
+                
+                # Store the data in the detObj dictionary
+                detObj = {"numObj": numDetectedObj, "x": x, "y": y, "z": z, "velocity":velocity}
+                dataOK = 1
+                
+ 
+        # Remove already processed data
+        if idX > 0 and byteBufferLength>idX:
+            shiftSize = totalPacketLen
+            
+                
+            byteBuffer[:byteBufferLength - shiftSize] = byteBuffer[shiftSize:byteBufferLength]
+            byteBuffer[byteBufferLength - shiftSize:] = np.zeros(len(byteBuffer[byteBufferLength - shiftSize:]),dtype = 'uint8')
+            byteBufferLength = byteBufferLength - shiftSize
+            
+            # Check that there are no errors with the buffer length
+            if byteBufferLength < 0:
+                byteBufferLength = 0         
+
+    return dataOK, frameNumber, detObj
+# ------------------------------------------------------------------
+
+# Funtion to update the data and display in the plot
+def update():
+    global mavlink_x, mavlink_y, mavlink_z
+    dataOk = 0
+    global detObj
+   
+      
+    # Read and parse the received data
+    dataOk, frameNumber, detObj = readAndParseData18xx(Dataport, configParameters)
+    
+    if dataOk and len(detObj["x"])>0:
+        print(detObj)
+        mavlink_y = -detObj["x"]
+        mavlink_x = detObj["y"]
+        mavlink_z = detObj["z"]
+        
+          
+    return dataOk
+
+
+# -------------------------    MAIN   -----------------------------------------  
+
+# Configurate the serial port
+CLIport, Dataport = serialConfig(configFileName)
+
+# Get the configuration parameters from the configuration file
+configParameters = parseConfigFile(configFileName)
+   
+# Main loop 
+detObj = {}  
+frameData = {}    
+currentIndex = 0
+while True:
+    try:
+        # Update the data and check if the data is okay
+        dataOk = update()
+        
+        if dataOk:
+            # Store the current frame into frameData
+            frameData[currentIndex] = detObj
+            currentIndex += 1
+        
+        time.sleep(0.05) # Sampling frequency of 30 Hz
+        
+    # Stop the program and close everything if Ctrl + c is pressed
+    except KeyboardInterrupt:
+        CLIport.write(('sensorStop\n').encode())
+        CLIport.close()
+        Dataport.close()
+        break
+        
+    
